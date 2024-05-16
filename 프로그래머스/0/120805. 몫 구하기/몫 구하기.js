f52a12c80b61459ddaebfecfe6533329d7b7fd78
박순호 <167044326+shpark694576@users.noreply.github.com>
2024-05-16 09:36:44 +0900

    [level 0] Title: 몫 구하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
@@ -0,0 +1,11 @@
+function solution(num1, num2) {
+    var answer = 0;
+    return answer;
+}
+
+function solution(num1, num2) {
+    var answer = 0;
+    answer = parseInt(num1/num2);
+    
+    return answer;
+}

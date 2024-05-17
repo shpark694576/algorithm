92b6df999473a42fd3b5176c83624ca2b299f789
박순호 <167044326+shpark694576@users.noreply.github.com>
2024-05-17 09:28:03 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.js
@@ -0,0 +1,18 @@
+function solution(age) {
+    var answer = 0;
+    return answer;
+}
+
+ 
+ 	
+function solution(birthYear) {
+    const currentYear = 2023;
+    return currentYear - birthYear;
+}
+
+// 테스트 케이스
+console.log(solution(1983));  
+console.log(solution(2000));
+
+
+

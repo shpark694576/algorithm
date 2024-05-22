dea9c50f1d72110702ff3306d9c96f99cf3f6fef
박순호 <167044326+shpark694576@users.noreply.github.com>
2024-05-22 10:00:49 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -0,0 +1,12 @@
+function solution(num1, num2) {
+    var answer = 0;
+    return answer;
+}
+function solution(num1, num2) {
+  return Math.floor((num1 / num2) * 1000);
+}
+
+// Math.trunc()는 소수점 이하는 다 버린다.
+function solution(num1, num2) {
+  return Math.trunc((num1 / num2) * 1000);
+}

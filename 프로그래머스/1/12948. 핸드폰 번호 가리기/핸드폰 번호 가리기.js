f0f060b5cb89834fb4eb9ae1b0346d7bb6534791
박순호 <167044326+shpark694576@users.noreply.github.com>
2024-06-27 15:15:00 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,10 @@
+function solution(phone_number) {
+    var answer = '';
+    return answer;
+}
+function solution(num) {
+    const numLength = num.length - 4;
+    
+    return "*".repeat(numLength)+num.substring(numLength);
+
+}

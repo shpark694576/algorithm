90b1fee6efe84631a1a598488bacc015f34316ee
박순호 <167044326+shpark694576@users.noreply.github.com>
2024-07-10 15:25:51 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -0,0 +1,13 @@
+function solution(a, b) {
+    var answer = 1234567890;
+    return answer;
+}
+function solution(a, b) {
+    let answer = 0;
+    
+    for(let i=0; i < a.length; i++){
+        answer += a[i]*b[i];
+    }
+    
+    return answer;
+}
